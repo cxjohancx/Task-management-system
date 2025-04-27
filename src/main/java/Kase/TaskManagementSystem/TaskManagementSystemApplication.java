c5bd07bfc1876d2b76968f459ddced4bf7c72cca
Johan <johanestebanrios11@gmail.com>
2025-04-27 12:02:13 -0500

    Creation of project

---
+++ src/main/java/Kase/TaskManagementSystem/TaskManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package Kase.TaskManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskManagementSystemApplication.class, args);
+	}
+
+}
